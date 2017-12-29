b9fc5132820a873cb8bb8648bd4479f8b4f9b180
amaoM <chapa0106@gmail.com>
2017-12-29 12:04:30 +0900

    Add react redux router

--- src/app.js
+++ src/app.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
 import { createStore } from 'redux';
-import Runway from './components/Runway';
+import { Provider } from 'react-redux';
+import { syncHistoryWithStore } from 'react-router-redux';
 import reducer from './reducers/index';
+import Runway from './components/Runway';
+import VisibleIncidentDetailContainer from './containers/IncidentDetailContainer';
+
+
+const store = createStore(reducer);
+
+const history = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render(
-  <Provider store={createStore(reducer)}>
-    <Runway />
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={Runway} />
+      <Route path="/detail/:id" component={VisibleIncidentDetailContainer} />
+    </Router>
   </Provider>,
   document.getElementById('root'),
 );
