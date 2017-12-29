6810b6b4fe658209454b523c894533d3431b14da
amaoM <chapa0106@gmail.com>
2017-12-29 12:02:41 +0900

    Delete unnecessary codes

--- src/reducers/incident.js
+++ src/reducers/incident.js
@@ -3,18 +3,6 @@ const incident = (
   action,
 ) => {
   switch (action.type) {
-    case 'STATUS_TOGGLE':
-      if (state.id === action.id) {
-        const toggleStatus = { up: 'down', down: 'up' };
-        return {
-          id: state.id,
-          status: toggleStatus[state.status],
-          title: state.title,
-          person: state.person,
-          toggleStatus: state.status,
-        };
-      }
-      return state;
     case 'CREATE_INCIDENT':
       return {
         title: action.title,
