582043b5750fde474d1a2bdb7b9ec0a3afea2768
amaoM <chapa0106@gmail.com>
2018-04-07 15:43:58 +0900

    Delete

--- src/middlewares/sample.js
+++
@@ -1,7 +0,0 @@
-const sampleMiddleware = store => next => action => {
-  console.log('next state', store.getState());
-  console.log('dispatch', store.dispatch);
-  const result = next(action);
-};
-
-export default sampleMiddleware;
